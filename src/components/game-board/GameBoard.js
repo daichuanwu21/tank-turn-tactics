6913af30a6b9941333517438754fcdc78889409a
daichuanwu21 <109904112+daichuanwu21@users.noreply.github.com>
2023-06-18 12:16:48 +1000

    Added dynamic map view and MUI

--- src/components/game-board/GameBoard.js
+++ src/components/game-board/GameBoard.js
@@ -1,24 +1,62 @@
 import { useSelector } from "react-redux";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Grid from "./Grid";
 import Tank from "./Tank";
 
 export default function GameBoard() {
   const tanks = useSelector((state) => state.tanks);
   return (
-    <div style={{ position: "absolute" }}>
-      <Grid />
-      {tanks.map((tank) => {
-        return (
-          <Tank
-            key={tank.uuid}
-            coordinateX={tank.coordinateX}
-            coordinateY={tank.coordinateY}
-            range={tank.range}
-            health={tank.health}
-            ap={tank.ap}
-          />
-        );
-      })}
+    <div
+      style={{
+        position: "relative", // Allow this div to act as parent div for children with absolute positioning
+      }}
+    >
+      <TransformWrapper>
+        <TransformComponent
+          wrapperStyle={{
+            maxWidth: "100%",
+            maxHeight: "calc(100vh - 48px)", // Account for bottom accordion
+          }}
+        >
+          <div>
+            <Grid />
+            {tanks.map((tank) => {
+              return (
+                <Tank
+                  key={tank.uuid}
+                  coordinateX={tank.coordinateX}
+                  coordinateY={tank.coordinateY}
+                  range={tank.range}
+                  health={tank.health}
+                  ap={tank.ap}
+                />
+              );
+            })}
+          </div>
+        </TransformComponent>
+      </TransformWrapper>
+      <Accordion
+        sx={{
+          position: "fixed",
+          width: "100%",
+          bottom: 0,
+        }}
+      >
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
     </div>
   );
 }
